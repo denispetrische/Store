c078891437590ce3dd79b2d696836a8f0a65a7df
0298287677@mail.ru <0298287677@mail.ru>
2023-07-19 15:35:44 +0300

    Created Stored Procedures for HistoryNote

---
+++ Store.Web/Scripts/StoredProcedures/HistoryNote/dbo.CreateHistoryNoteProcedure.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[CreateHistoryNote]
+	@Id uniqueidentifier = '',
+	@Message nvarchar(Max) = '',
+	@Date nvarchar(Max) = '',
+	@UserGuid uniqueidentifier = ''
+AS
+BEGIN
+	INSERT INTO HistoryNotes([Id],[Message],[Date],[UserGuid])
+	                  VALUES(@Id, @Message, @Date, @UserGuid)
+END;
