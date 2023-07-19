c853f4553b652366e983c1f288296b98aaef0ff7
0298287677@mail.ru <0298287677@mail.ru>
2023-07-19 16:47:17 +0300

    ProductRepo Implemented

--- Store.Web/Data/ProductRepo.cs
+++ Store.Web/Data/ProductRepo.cs
@@ -1,33 +1,67 @@
-﻿using Store.Web.Abstractions.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Web.Abstractions.Data;
 using Store.Web.Models;
 
 namespace Store.Web.Data
 {
     public class ProductRepo : IProductRepo
     {
+        private readonly StoreWebContext _context;
+
+        public ProductRepo(StoreWebContext context)
+        {
+            _context = context;
+        }
+
         public Task CreateProduct(Product product)
         {
-            throw new NotImplementedException();
+            _context.Database.ExecuteSqlRaw($"CreateProduct {product.Id} " +
+                                                          $"{product.Name} " +
+                                                          $"{product.Description} " +
+                                                          $"{product.IsOnTrade} " +
+                                                          $"{product.ReceiptDate} " +
+                                                          $"{product.ExpireDate} " +
+                                                          $"{product.Amount} " +
+                                                          $"{product.Price} " +
+                                                          $"{product.Currency}");
+
+            return Task.CompletedTask;
         }
 
         public Task DeleteProductById(string id)
         {
-            throw new NotImplementedException();
+            _context.Database.ExecuteSqlRawAsync($"DeleteProductById {id}");
+
+            return Task.CompletedTask;
         }
 
         public Task<Product> GetProductById(string id)
         {
-            throw new NotImplementedException();
+            Product product = _context.Products.FromSqlRaw($"GetProductById {id}").FirstOrDefault();
+
+            return Task.FromResult(product);
         }
 
         public Task<List<Product>> GetProducts()
         {
-            throw new NotImplementedException();
+            var products = _context.Products.FromSqlRaw("GetProducts").ToList();
+
+            return Task.FromResult(products);
         }
 
         public Task UpdateProduct(Product product)
         {
-            throw new NotImplementedException();
+            _context.Database.ExecuteSqlRaw($"UpdateProduct {product.Id} " +
+                                                          $"{product.Name} " +
+                                                          $"{product.Description} " +
+                                                          $"{product.IsOnTrade} " +
+                                                          $"{product.ReceiptDate} " +
+                                                          $"{product.ExpireDate} " +
+                                                          $"{product.Amount} " +
+                                                          $"{product.Price} " +
+                                                          $"{product.Currency}");
+
+            return Task.CompletedTask;
         }
     }
 }
