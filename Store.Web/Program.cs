92253339d80d351c2a92e2820c5d980f25b5f12a
0298287677@mail.ru <0298287677@mail.ru>
2023-07-19 10:44:26 +0300

    Added seeder PrepDb

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Store.Web.Data;
+using Store.Web.Seeders;
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("StoreWebContextConnection") ?? throw new InvalidOperationException("Connection string 'StoreWebContextConnection' not found.");
 
@@ -8,6 +10,7 @@ builder.Services.AddDbContext<StoreWebContext>(options =>
     options.UseSqlServer(connectionString));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<StoreWebContext>();
 
 // Add services to the container.
@@ -35,5 +38,7 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
+
+PrepDb.PrepDbInfo(app, app.Environment.IsProduction()); // Seeder
 
 app.Run();
---
+++ Store.Web/Seeders/PrepDb.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.CodeAnalysis;
+using Store.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Store.Web.Seeders
+{
+    public static class PrepDb
+    {
+        public static void PrepDbInfo(IApplicationBuilder app, bool isProd)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<StoreWebContext>(), isProd);
+            }
+        }
+
+        private static void SeedData(StoreWebContext context, bool isProd)
+        {
+            if (isProd)
+            {
+                Console.WriteLine("Attempting to apply migrations");
+
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Could not run migrations {e.Message}");
+                }
+            }
+
+            if (!context.Users.Any())
+            {
+                Console.WriteLine("Seeding Data");
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Data already in Db");
+            }
+        }
+    }
+}
