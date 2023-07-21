e07accd9aa45f63dceede8072e5fe25b6ee43cc9
0298287677@mail.ru <0298287677@mail.ru>
2023-07-21 13:43:50 +0300

    Added BuyProduct Function

--- Store.Web/Controllers/MarketController.cs
+++ Store.Web/Controllers/MarketController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Store.Web.Abstractions.Data;
 using Store.Web.Dtos.Product;
@@ -9,18 +10,55 @@ namespace Store.Web.Controllers
     public class MarketController : Controller
     {
         private readonly IMapper _mapper;
-        private readonly IProductRepo _repo;
+        private readonly IProductRepo _productRepo;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IHistoryNoteRepo _historyNoteRepo;
 
-        public MarketController(IMapper mapper, IProductRepo repo)
+        public MarketController(IMapper mapper, IProductRepo repo, UserManager<IdentityUser> userManager, IHistoryNoteRepo historyNoteRepo)
         {
             _mapper = mapper;
-            _repo = repo;
+            _productRepo = repo;
+            _userManager = userManager;
+            _historyNoteRepo = historyNoteRepo;
         }
 
         public async Task<IActionResult> MainView()
         {
-            var products = await _repo.GetProductsForMarket();
+            var products = await _productRepo.GetProductsForMarket();
             return View(_mapper.Map<List<Product>, List<ProductMarketViewDto>>(products));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BuyProduct([FromQuery(Name = "param1")] Guid? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("MainView");
+            }
+
+            var product = _productRepo.GetProductById(id.Value.ToString()).Result;
+
+            if (product == null)
+            {
+                return RedirectToAction("MainView");
+            }
+
+            if (product.Amount > 0)
+            {
+                product.Amount = product.Amount - 1;
+
+                HistoryNote note = new HistoryNote()
+                {
+                    Message = $"Продукт {product.Name} был куплен. Пользователь:{User.Identity.Name}",
+                    Date = DateTime.Now,
+                    UserId = _userManager.GetUserId(HttpContext.User)
+                };
+
+                _historyNoteRepo.CreateHistoryNote(note);
+                _productRepo.UpdateProduct(product);
+            }
+
+            return RedirectToAction("MainView");
         }
     }
 }
