c13677c27bcba83d907d728cb50eee3486fa134a
0298287677@mail.ru <0298287677@mail.ru>
2023-07-21 14:29:53 +0300

    Added Logging for StoreController

--- Store.Web/Controllers/StoreController.cs
+++ Store.Web/Controllers/StoreController.cs
@@ -17,19 +17,37 @@ namespace Store.Web.Controllers
         private readonly IHistoryNoteRepo _repoHistory;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly AppConstants _constants;
+        private readonly ILogger<StoreController> _logger;
 
-        public StoreController(IMapper mapper, IProductRepo repoProduct, IHistoryNoteRepo repoHistory, UserManager<IdentityUser> userManager)
+        public StoreController(IMapper mapper, 
+                               IProductRepo repoProduct, 
+                               IHistoryNoteRepo repoHistory, 
+                               UserManager<IdentityUser> userManager,
+                               ILogger<StoreController> logger)
         {
             _mapper = mapper;
             _repoProduct = repoProduct;
             _repoHistory = repoHistory;
             _userManager = userManager;
             _constants = new AppConstants();
+            _logger = logger;
         }
 
         public async Task<IActionResult> MainView()
         {
-            var products = await _repoProduct.GetProducts();
+            List<Product> products = null;
+
+            try
+            {
+                products = await _repoProduct.GetProducts();
+                _logger.LogInformation($"Products successfully gotten");
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Can't get products from products repo. Reason: {e.Message}");
+            }
+
             return View(_mapper.Map<List<Product>,List<ProductStoreViewDto>>(products));
         }
 
@@ -38,10 +56,22 @@ namespace Store.Web.Controllers
         {
             if (id == null)
             {
+                _logger.LogInformation("ChangeIsOnTrade: id == null");
                 return RedirectToAction("MainView");
             }
 
-            var product = await _repoProduct.GetProductById(id.Value.ToString());
+            Product product = null;
+
+            try
+            {
+                product = await _repoProduct.GetProductById(id.Value.ToString());
+                _logger.LogInformation("ChangeIsOnTrade: product was received");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"ChangeIsOnTrade: product was not received. Reason: {e.Message}");
+            }
+            
             if (product == null)
             {
                 return RedirectToAction("MainView");
@@ -52,7 +82,16 @@ namespace Store.Web.Controllers
             if (product.IsOnTrade)
             {
                 product.IsOnTrade = !product.IsOnTrade;
-                _repoProduct.UpdateProduct(product);
+
+                try
+                {
+                    _repoProduct.UpdateProduct(product);
+                    _logger.LogInformation($"ChangeIsOnTrade: product was successfully updated");
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError($"ChangeIsOnTrade: unable to update product, Reason: {e.Message}");
+                }
 
                 note = new HistoryNote()
                 {
@@ -61,7 +100,15 @@ namespace Store.Web.Controllers
                     UserId = _userManager.GetUserId(HttpContext.User)
                 };
 
-                _repoHistory.CreateHistoryNote(note);
+                try
+                {
+                    _repoHistory.CreateHistoryNote(note);
+                    _logger.LogInformation($"ChangeIsOnTrade: note was succesfully created");
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError($"ChangeIsOnTrade: unable to create note, Reason: {e.Message}");
+                }                
 
                 return RedirectToAction("MainView");
             }
@@ -80,8 +127,16 @@ namespace Store.Web.Controllers
                 UserId = _userManager.GetUserId(HttpContext.User)
             };
 
-            _repoHistory.CreateHistoryNote(note);
-            _repoProduct.UpdateProduct(product);
+            try
+            {
+                _repoHistory.CreateHistoryNote(note);
+                _repoProduct.UpdateProduct(product);
+                _logger.LogInformation($"ChangeIsOnTrade: product was successfulyy updated and note was succesfully created");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"ChangeIsOnTrade: unable to create note or update product, Reason: {e.Message}");
+            }
 
             return RedirectToAction("MainView");
         }
@@ -95,12 +150,20 @@ namespace Store.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> AddProduct([FromForm] ProductCreateDto productCreateDto)
         {
-            Console.WriteLine("Here!!!");
             var product = _mapper.Map<Product>(productCreateDto);
             product.ReceiptDate = DateTime.Now;
             product.ExpireDate = DateTime.Now.Add(_constants._expireTime);
 
-            _repoProduct.CreateProduct(product);
+            try
+            {
+                _repoProduct.CreateProduct(product);
+                _logger.LogInformation("AddProduct: product was succesfully created");
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"AddProduct: unable to create product. Reason: {e.Message}");
+            }
 
             return RedirectToAction("MainView");
         }
