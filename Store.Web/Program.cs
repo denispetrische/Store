99fd3ece94551d5a8e92c4271140bc45f77233eb
0298287677@mail.ru <0298287677@mail.ru>
2023-07-24 10:29:52 +0300

    Added Database.Migrate

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -36,6 +36,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<StoreWebContext>();
+    context.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
